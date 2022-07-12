ed32ab064901170d66595030b3547058b064be49
Guillem Cordoba <guillem.cordoba@gmail.com>
2022-07-12 12:28:47 +0200

    Exported resource

--- src/lib.rs
+++ src/lib.rs
@@ -13,5 +13,6 @@ pub use battle::Battle;
 pub use bot::{Bot, BotStrategy, ColorConfig};
 pub use broken_bots::{blue::blue, grey::grey, red::red, yellow::yellow};
 pub use direction::{Direction, Rotation};
+pub use resource::Resource;
 pub use ruscii::terminal::Color;
 pub use state::{GameState, Position};
